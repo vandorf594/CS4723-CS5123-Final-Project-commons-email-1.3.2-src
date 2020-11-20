f7ba4368033ca95e519990ce2aea9d7da3591027
vandorf594 <vandorf594@gmail.com>
2020-11-20 13:30:26 -0600

    Added Test1

---
+++ src/test/java/org/apache/commons/mail/Test1.java
@@ -0,0 +1,27 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class Test1 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	public void testAddBcc1() throws EmailException {
+		/*
+		 * Adding 4 bcc emails using the addBcc() method
+		 * and checking to see that the 4 emails are stored
+		 */
+		String[] fourTestEmails = {"a@b.com", "a@c.com", "a@d.com", "a@e.com"};
+		testEmail.addBcc(fourTestEmails);
+		assertEquals("value should be a@b.com","a@b.com", testEmail.getBccAddresses().get(0).toString());
+		assertEquals("value should be a@c.com","a@c.com", testEmail.getBccAddresses().get(1).toString());
+		assertEquals("value should be a@d.com","a@d.com", testEmail.getBccAddresses().get(2).toString());
+		assertEquals("value should be a@e.com","a@e.com", testEmail.getBccAddresses().get(3).toString());
+	}
+
+}
